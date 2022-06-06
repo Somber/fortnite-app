6f0753c8f035098d3b5bcdb78134d66dfa2a69dd
Somber <somber_@msn.com>
2022-06-06 08:59:16 +0000

    cambio en el tratamiento de las imagenes

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import {Card} from 'react-bootstrap';
-import deathImg from '../images/deaths.jpg';
-import killImg from '../images/kills.jpg';
 
 export default function Score({kills, deaths}) {
   return (
     <>
       <Card.Text className='card_score'>
         <Card.Text>
-          <img className='score_img' src={killImg} alt="Las veces que ha matado"></img> <span className='score_text'>{kills}</span>       
+          <img className='score_img' src={require('../images/kills.jpg')} alt="Las veces que ha matado"></img> <span className='score_text'>{kills}</span>       
         </Card.Text>  
         <Card.Text>
-          <img className='score_img' src={deathImg} alt="Las veces que ha muerto"></img> <span className='score_text'>{deaths}</span>       
+          <img className='score_img' src={require('../images/deaths.jpg')}></img> <span className='score_text'>{deaths}</span>       
         </Card.Text>  
       </Card.Text>
     </>
