44cbaa67a61008045e44ade21359b637622fd1b6
Somber <somber_@msn.com>
2022-06-08 20:29:52 +0000

    agregado tooltip a imagenes

--- src/components/ScoreOverall.js
+++ src/components/ScoreOverall.js
@@ -4,6 +4,7 @@ import deathImg from '../images/deaths.png';
 import killImg from '../images/kills.png';
 import winImg from '../images/wins.png';
 import matchImg from '../images/matches.png';
+import ReactTooltip from 'react-tooltip';
 
 export default function ScoreOverall({overall}) {
   return (
@@ -13,46 +14,54 @@ export default function ScoreOverall({overall}) {
           <Col>
             <Row>
               <Col>
-                <img className='score_img' src={matchImg} alt="Partidas jugadas"></img>
+                <img data-tip="Partidas jugadas" className='score_img' src={matchImg} alt="Partidas jugadas"></img>
               </Col>
             </Row>
             <Row>
               <Col>
-                <span className='score_text '>{overall ? overall.matches : 0}</span>
+                <span data-tip="Partidas jugadas" className='score_text '>{overall ? overall.matches : 0}</span>
               </Col>
             </Row>
           </Col>
           <Col>
             <Row>
               <Col>
-                <img className='score_img' src={winImg} alt="Partidas ganadas"></img>
+                <img data-tip="Partidas ganadas" className='score_img' src={winImg} alt="Partidas ganadas"></img>
               </Col>
             </Row>
             <Row>
               <Col>
-                <span className='score_text'>{overall ? overall.wins : 0}</span>          
+                <span data-tip="Partidas ganadas" className='score_text'>{overall ? overall.wins : 0}</span>          
               </Col>
             </Row>
           </Col>
           <Col>
             <Row>
               <Col>
-                <img className='score_img' src={killImg} alt="Bajas realizadas"></img>
+                <img data-tip="Bajas realizadas" className='score_img' src={killImg} alt="Bajas realizadas"></img>
               </Col>
             </Row>
             <Row>
               <Col>
-                <span className='score_text'>{overall ? overall.kills : 0}</span> 
+                <span data-tip="Bajas realizadas" className='score_text'>{overall ? overall.kills : 0}</span> 
               </Col>
             </Row>
-            
           </Col>
           <Col>
-            <img className='score_img' src={deathImg} alt="Muertes"></img>
-            <span className='score_text'>{overall ? overall.deaths : 0}</span> <br/>
+            <Row>
+              <Col>
+                <img data-tip="Muertes" className='score_img' src={deathImg} alt="Muertes"></img>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span data-tip="Muertes" className='score_text'>{overall ? overall.deaths : 0}</span> 
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
+      <ReactTooltip />
     </>
   )
 }
