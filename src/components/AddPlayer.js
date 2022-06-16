3ee057bd3ae9f5ff788e0081cb9c289bff7e9e27
Somber <somber_@msn.com>
2022-06-16 16:27:57 +0000

    ajustes boton add player

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -12,41 +12,43 @@ const AddPlayer = ({setPlayers}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (inputValue.length > 0){
+          getPlayer(inputValue)
+          .then(p => {
+              const playersTmp = localStorage.getItem('playersV2');
+              let players;
 
-        getPlayer(inputValue)
-            .then(p => {
-                const playersTmp = localStorage.getItem('playersV2');
-                let players;
+              if (!playersTmp){
+                  players = [p];
 
-                if (!playersTmp){
-                    players = [p];
+              } else {
+                  players = [p, ...JSON.parse(playersTmp)];
 
-                } else {
-                    players = [p, ...JSON.parse(playersTmp)];
+              }
 
+              const copyListItems = [...players];
+              copyListItems.sort((a,b)=>{
+                if (a.overall.kd > b.overall.kd) {
+                  return 1;
                 }
+                if (a.overall.kd < b.overall.kd) {
+                  return -1;
+                }
+                return 0;
+              });
+              copyListItems.reverse();
+              const kdPlayer = copyListItems[0]
 
-                const copyListItems = [...players];
-                copyListItems.sort((a,b)=>{
-                  if (a.overall.kd > b.overall.kd) {
-                    return 1;
-                  }
-                  if (a.overall.kd < b.overall.kd) {
-                    return -1;
-                  }
-                  return 0;
-                });
-                copyListItems.reverse();
-                const kdPlayer = copyListItems[0]
+              const t = players.map((pus)=>{
+                pus.kd = kdPlayer.name === pus.name;
+                return pus;
+              });
 
-                const t = players.map((pus)=>{
-                  pus.kd = kdPlayer.name === pus.name;
-                  return pus;
-                });
+              localStorage.setItem('playersV2', JSON.stringify(t));
+              setPlayers(players);
+          });
+        }
 
-                localStorage.setItem('playersV2', JSON.stringify(t));
-                setPlayers(players);
-            });
 
         setinputValue("");
     }
